ed59f82eea3f1b086f3a8cda2249056f1fa8907d
Leandro Bezerra Marinho <leandrobezerramarinho@gmail.com>
2015-03-17 19:17:48 -0300

    Bayes

--- Aprendizagem de Maquina/Implementacoes/bayes/runBayes.m
+++ Aprendizagem de Maquina/Implementacoes/bayes/runBayes.m
@@ -1,8 +1,8 @@
 close all; clear all; clc
-addpath('bayes');
+addpath('..');
 
 %% Carregando os dados
-data = load('iris.data');
+data = load('../iris.data');
 dataset.x = data(:, 1:4);
 dataset.y = data(:, 5:7);
 clear data;
@@ -24,23 +24,23 @@ K = 0;
 alg = 'bayes';
 subplot(lin, col, 1)
 decisionSurface([0 6 3 9], alg, dataset, K, [2 1])
-ylabel('T. Sépala', 'FontSize', 14);
+ylabel('C. Sépala', 'FontSize', 14);
 xlabel('L. Sépala', 'FontSize', 14);
 
 subplot(lin, col, 2)
 decisionSurface([0 8 3 9], alg, dataset, K, [3 1])
-ylabel('T. Sépala', 'FontSize', 14);
-xlabel('T. Pétala', 'FontSize', 14);
+ylabel('C. Sépala', 'FontSize', 14);
+xlabel('C. Pétala', 'FontSize', 14);
 
 subplot(lin, col, 3)
 decisionSurface([-1 5 -1 9], alg, dataset, K, [4 1])
-ylabel('T. Sépala', 'FontSize', 14);
+ylabel('C. Sépala', 'FontSize', 14);
 xlabel('L. Pétala', 'FontSize', 14);
 
 subplot(lin, col, 4)
 decisionSurface([-2 8 -1 9], alg, dataset, K, [3 2])
 ylabel('L. Sépala', 'FontSize', 14);
-xlabel('T. Pétala', 'FontSize', 14);
+xlabel('C. Pétala', 'FontSize', 14);
 
 subplot(lin, col, 5)
 decisionSurface([-2 8 1 5], alg, dataset, K, [4 2])
@@ -49,5 +49,5 @@ xlabel('L. Pétala', 'FontSize', 14);
 
 subplot(lin, col, 6)
 decisionSurface([-2 8 -1 9], alg, dataset, K, [4 3])
-ylabel('T. Pétala', 'FontSize', 14);
+ylabel('C. Pétala', 'FontSize', 14);
 xlabel('L. Pétala', 'FontSize', 14);
