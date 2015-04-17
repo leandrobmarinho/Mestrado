3541855f65f895d229bb5f9e512922e0bc410ece
Leandro Bezerra Marinho <leandrobezerramarinho@gmail.com>
2015-04-17 18:56:30 -0300

    carregaDados

--- implementacoes/carregaDados.m
+++ implementacoes/carregaDados.m
@@ -3,13 +3,61 @@ function [ dataset ] = carregaDados( path, tipo_saida)
 %   Detailed explanation goes here
 
 %% Carregando os dados
-data = load(strcat('../dados/', path));
-dataset.x = data(:, 1:end-1);
-y = data(:, end);
+if (strcmpi('HAR', path) == 1)
+    fprintf('Carregando dados HAR...\n')
+    
+    
+    %%Tenta uma selecao de atributos
+    [col, desc] = textread('../dados/HAR/features.txt', '%d %s');
+    colunas = [];
+    for i = 1 : length(desc)
+        
+        if ( isempty( findstr(desc{i}, 'std()')) && isempty( findstr(desc{i}, 'min()')) ...
+                && isempty( findstr(desc{i}, 'max()')))
+            colunas = [colunas; col(i)];
+        end
+        
+    end
+    
+    
+    %%
+    y = load('../dados/HAR/test/y_test.txt');
+    data = load('../dados/HAR/test/X_test.txt');
+    data = data(:, colunas);
+    
+    prc = 0.3;
+    ys = []; xs = [];
+    for i = 1 : length(unique(y))
+        
+        ind = find(y == i);
+        
+        tamClasse = length(ind);
+        limite = floor(prc*tamClasse);
+        ind = ind(randperm(tamClasse));
+        
+        ys = [ys; y(ind(1:limite), :)];
+        xs = [xs; data(ind(1:limite), :)];
+        
+    end
+    y = ys;
+    dataset.x = xs;
+    
+    
+%     dataset.x = [data; load('../dados/HAR/train/X_train.txt')];
+%     y = [data; load('../dados/HAR/train/y_train.txt')];
+    
+else
+    data = load(strcat('../dados/', path));
+    dataset.x = data(:, 1:end-1);
+    y = data(:, end);
+    
+end
 clear data;
 
 %% Normalizando os dados
-dataset.x = normalizaDados(dataset.x, 1);
+if (strcmpi('HAR', path) == 0)
+    dataset.x = normalizaDados(dataset.x, 1);
+end
 
 if (tipo_saida == 1)
     %% pre-processing / 1-of-S output encoding scheme
