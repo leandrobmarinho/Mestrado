6e2518b9f709ed2ec70416d72e69f95a37b790c1
Leandro Bezerra Marinho <leandrobezerramarinho@gmail.com>
2015-05-11 17:57:04 -0300

    MLP

--- implementacoes/MLP/buscaTopologiaMLP.m
+++ implementacoes/MLP/buscaTopologiaMLP.m
@@ -2,41 +2,40 @@ function [ resultado ] = buscaTopologiaMLP( dados, params, conf )
 %BUSCATOPOLOGIASOM Summary of this function goes here
 %   Detailed explanation goes here
 
-save('topologiaSOM');
-errosTrein = zeros(conf.treinos, length(params));
-errosTeste = zeros(conf.treinos, length(params));
+save('topologiaMLP');
+acc = zeros(conf.treinos, length(params));
 for i = 1 : conf.treinos,
     %% Embaralhando os dados
     [learnPoints{i}, testData{i}] = embaralhaDados(dados, conf.ptrn, 2);
     
     for j = 1: length(params),
         tic
-        fprintf('Busca Topologia SOM. %d - %d.\n', i, j);
-        [modelo{i,j}, errosQuantTrein{i,j}] = trainSOM(learnPoints{i}, params{j});
+        fprintf('Busca Topologia MLP. %d - %d.\n', i, j);
+        [modelo{i,j}] = treinoMLP(learnPoints{i}, params{j});
         tempoTrein(i,j) = toc;
-        errosTrein(i, j) = mean(errosQuantTrein{i,j});
         
-        [errosTeste(i, j)] = testeSOM(modelo{i,j}, testData{i});        
+        [Yh] = testeMLP(modelo{i,j}, testData{i});        
         
-        fprintf('ErroTrein: %f. ErroTeste: %f\n', errosTrein(i, j), errosTeste(i, j));
+        [~, target] = max(testData{i}.y');
+        confusionMatrices{i, j} = confusionmat(target, Yh);
+        acc(i,j) = trace(confusionMatrices{i, j}) / length(target);
+                
+        fprintf('ACC: %f\n',  acc(i, j));
         
-        save('topologiaSOM','-append');
+        save('topologiaMLP','-append');
         
     end
 end
 
 if conf.treinos == 1
-    resultado.varianciaTrein = zeros(1, size(errosTrein,2));
-    resultado.varianciaTeste = zeros(1, size(errosTeste,2));
+    resultado.varianciaTeste = zeros(1, size(acc,2));
 else
-    resultado.varianciaTrein = var(errosTrein, 1);
-    resultado.varianciaTeste = var(errosTeste, 1);
+    resultado.varianciaTeste = var(acc, 1);
 end
-resultado.mediaTrein = mean(errosTrein, 1);
-resultado.mediaTeste = mean(errosTeste, 1);
+resultado.mediaTeste = mean(acc, 1);
 resultado.tempoTreino = tempoTrein;
 
-save('topologiaSOM','-append');
+save('topologiaMLP','-append');
 
 end
 
--- implementacoes/MLP/run.m
+++ implementacoes/MLP/run.m
@@ -1,19 +1,40 @@
 close all; clear all; clc; addpath('..');
 
 %% Pré-processamento
-conf.pca = 0.98; conf.selecaoAtr = 0.1; %0.3
-dataset = carregaDados('har', 1, conf);
+valores = [ 0.001 0.005 0.007 0.01];
+%81p [ 0.001 0.005 *0.007 0.01] 40
+%78p [ *0.001 0.005 0.007 0.01] 50
+% [ 0.001 0.005 0.007 0.01]
+%84p [ 0.001 *0.005 0.01 0.1 ] 60
+%82p [ 0.001 0.005 *0.007 0.01] 60
+%80p [ 0.001 *0.005 0.007 0.01] 80
 
-%% Configurações gerais
-ptrn = 0.8;
-numRodadas = 1;
+for i = 1 : length(valores)
+    
+    conf.pca = 0.98;
+    conf.selecaoAtr = 0.03; %0 *0.03 *0.1 0.3 0.45
+    dataset = carregaDados('har', 1, conf);
+    
+    
+    %% Configurações gerais
+    ptrn = 0.8;
+    numRodadas = 1;
+    
+    % Topologia da rede
+    % floor((size(dataset.x, 2) + 6) /2)
+    % *50 30 *60 80 100
+    config.L = 55; %numero de neuronios na camada oculta
+    config.maxIterations = 150;
+    config.lRate = valores(i); %[0.1 *0.05 *0.01 0.03 0.001 0.005]
+    
+    mlp(dataset, ptrn, numRodadas, config)
+    acc(i) = ans.accMean;
+end
 
-% Topologia da rede
-config.L = 50; %numero de neuronios na camada oculta
-%20 30 50
-config.maxIterations = 100;
-config.lRate = 0.1; %0.05
-
-result = mlp(dataset, ptrn, numRodadas, config)
-
+acc
 somAlerta()
+
+
+%%
+% 20 30 50
+% 00 00 63
