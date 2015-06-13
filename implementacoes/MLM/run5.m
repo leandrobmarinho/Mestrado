2eed71c9881bc8f27d9aafaf11f90c93ca88aa68
Leandro Bezerra Marinho <leandrobezerramarinho@gmail.com>
2015-06-13 19:54:48 -0300

    mlm

---
+++ implementacoes/MLM/run5.m
@@ -0,0 +1,65 @@
+close all; clear all; clc; addpath('..');
+
+% Pré-processamento
+dataset = carregaDados('har', 1);
+
+
+%% Configurações gerais
+ptrn = 0.8;
+numRodadas = 5;
+
+%% MLM
+config.method = 'knn'; % lsqnonlin knn ''
+% for dist = {'mahalanobis', 'cityblock', ''}
+for dist = { 'mahalanobis'}
+
+%      for k = [0.1 0.5 1]
+    for k = [ 1]
+        config.k = k;
+        config.NN = 9;
+        config.distance = dist{1}; % mahalanobis cityblock ''
+        r = sim2MLM(dataset, ptrn, numRodadas, config);
+        
+        save(sprintf('mlm_%s_%d_D-%s_%.0f_all', config.method, config.NN,...
+            config.distance, config.k*100))
+        
+        fprintf(sprintf('mlm_%s_%d_D-%s_%.0f_all\n', config.method, config.NN,...
+            config.distance, config.k*100))
+    end    
+end
+
+% clear config
+% config.method = ''; % lsqnonlin knn ''
+% for dist = {'mahalanobis', 'cityblock', ''}
+% 
+%     for k = [0.1 0.5 1]
+%         config.k = k;
+%         config.distance = dist{1}; % mahalanobis cityblock ''
+%         r = sim2MLM(dataset, ptrn, numRodadas, config);
+%         
+%         save(sprintf('mlm_%s_%s_D-%s_%.0f_all', config.method, 'fsolve', ...
+%             config.distance, config.k*100))
+%         
+%         fprintf(sprintf('mlm_%s_%s_D-%s_%.0f_all\n', config.method, 'fsolve', ...
+%             config.distance, config.k*100))
+%     end
+%     
+% end
+
+% clear config
+% config.method = 'lsqnonlin'; % lsqnonlin knn ''
+% for dist = {'mahalanobis', 'cityblock', ''}
+% 
+%     for k = [0.1 0.5 1]
+%         config.k = k;
+%         config.distance = dist{1}; % mahalanobis cityblock ''
+%         r = simMLM(dataset, ptrn, numRodadas, config);
+%         
+%         save(sprintf('mlm_%s_%s_D-%s_%.0f_all', config.method, 'lsqnonlin', ...
+%             config.distance, config.k*100))
+%         
+%         fprintf(sprintf('mlm_%s_%s_D-%s_%.0f_all\n', config.method, 'lsqnonlin', ...
+%             config.distance, config.k*100))
+%     end
+%     
+% end
