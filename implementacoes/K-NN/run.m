f8bcbe4d41d3e2a7b735afa054d0813f705ff7fe
Leandro Bezerra Marinho <leandrobezerramarinho@gmail.com>
2015-06-15 14:20:02 -0300

    mlm

--- implementacoes/K-NN/run.m
+++ implementacoes/K-NN/run.m
@@ -2,13 +2,13 @@ close all; clear all; clc; addpath('../');
 
 %% Pré-processamento
 conf.pca = 0.98;
-conf.selecaoAtr = 0.1;
-dados = carregaDados('har', 1, conf);
+% conf.selecaoAtr = 0.1;
+dados = carregaDados('har', 1);
 
 
 %% Configurações gerais
 ptrn = 0.8;
-numRodadas = 10;
+numRodadas = 5;
 
 r = simKNN( dados, ptrn, numRodadas, 1)
 
